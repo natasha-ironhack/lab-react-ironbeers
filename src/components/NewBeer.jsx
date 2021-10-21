788c4595562e096c2801381b84e6756f62df611b
Natasha Smith <pisceanchic@gmail.com>
2021-10-21 16:05:39 +0200

    Iteration 1, 3, & 6 done

--- src/components/NewBeer.jsx
+++ src/components/NewBeer.jsx
@@ -1,4 +1,6 @@
+import axios from 'axios';
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
 class NewBeer extends Component {
   state = {
@@ -11,13 +13,36 @@ class NewBeer extends Component {
     this.setState({ [event.target.name]: event.target.value });
   };
 
+  handleSubmit = (event) => {
+    event.preventDefault();
+    //we are gonna send the info on the state to the POST
+    //endpoint
+    //the POST endpoint is the whole address.
+    //how u contact the API
+    //this is the endPoint that the backend will have
+    //that the server they build will have
+    //how to access those is with the request we send
+    //to the end point?
+    //this is a POST request. they always expect the info we will send
+    //how to send that? as an additional arg.
+    //add an , and an obj. {} after the link here
+    axios.post('https://ih-beers-api2.herokuapp.com/beers/new', {
+      name: this.state.name,
+      description: this.state.description,
+      contributed_by: this.state.contributedBy,
+    });
+  };
+
   render() {
     return (
       <div>
+        <Link to="/">
+          <h1>Home</h1>
+        </Link>
         <h1>Add Beer Form</h1>
         {/*need a control form(at) */}
 
-        <form>
+        <form onSubmit={this.handleSubmit}>
           <label htmlFor="name">name</label>
           <input
             onChange={this.handleChange}
