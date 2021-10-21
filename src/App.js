8b86187363e78e26df11be4ed328cfd81bfb2b6f
Natasha Smith <pisceanchic@gmail.com>
2021-10-21 15:39:01 +0200

    Iteration 1 & 3 done

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import React from 'react';
 import './App.css';
 import { Switch, Route } from 'react-router-dom';
 import Home from './components/Home';
+import AllBeers from './components/AllBeers';
 
 function App() {
   return (
     <div className="App">
       <Switch>
         <Route path="/" exact component={Home} />
+        <Route path="/beers" exact component={AllBeers} />
       </Switch>
     </div>
   );
---
+++ src/components/AllBeers.jsx
@@ -0,0 +1,46 @@
+import axios from 'axios';
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+
+class AllBeers extends Component {
+  state = {
+    listOfBeers: null,
+    isLoading: true,
+  };
+
+  componentDidMount() {
+    axios
+      .get('https://ih-beers-api2.herokuapp.com/beers')
+      .then((response) => {
+        console.log(response.data);
+        this.setState({ listOfBeers: response.data, isLoading: false });
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
+
+  render() {
+    return (
+      <div>
+        <Link to="/">
+          <h1>Home</h1>
+        </Link>
+        <h1>All Beers!</h1>
+
+        {this.state.isLoading && <h1>...Loading</h1>}
+        {!this.state.isLoading &&
+          this.state.listOfBeers.map((oneBeer) => {
+            return (
+              <div>
+                <img src={oneBeer.image_url} alt="" height="200px" />;
+                <h3>{oneBeer.name}</h3>
+              </div>
+            );
+          })}
+      </div>
+    );
+  }
+}
+
+export default AllBeers;
